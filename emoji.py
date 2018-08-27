d68c404d531a142ce33908348b162e89ed3f785a
sergio <s_alyoshkin@yahoo.com>
2018-08-27 19:15:44 +0300

    first commit

---
+++ emoji.py
@@ -0,0 +1,61 @@
+
+
+emoji = {'snowman':'☃️','pizza':'🍕','bikini':'👙','red heart':'❤️','orange heart':'🧡','yellow heart':'💛', \
+         'green heart':'💚','blue heart':'💙','purple heart':'💜','black heart':'🖤', \
+		 'blue drops':'💦','red dress':'👗','kiss':'💋', \
+        'tongue':'👅','blue wave':'🌊','smiling face':'🙂','grey heart':'❤','batterfly':'🦋','hot beverage':'☕', \
+		'camera':'📷','leaf fluttering':'🍃','computer':'💻','unicorn':'🦄','wolf face':'🐺','cherry blossom':'🌸', \
+		'glob with meridians':'🌐','last quater moon with face':'🌜','sparkling heart':'💖','ribbon':'🎀','':'', \
+		'rose':'🌹','cheese':'🧀','wilted flower':'🥀','gem stone brilliant jewerly':'💎','victory hand':'✌', \
+		'flexed bicept':'💪','seeding':'🌱','extraterrestrial alien':'👽','strawberry':'🍓','crab':'🦀', \
+		'heart with arrow':'💘','umbrella':'☂','basketball':'🏀','handball':'🤾','teacup without handle':'🍵', \
+		'heart eyes':'😍','sunflower':'🌻','rainbow':'🌈','revolving hearts':'💞','cloud with rain':'🌧️', \
+		'umbrella with rain':'☔','boot sniker':'👟','nail polish manicure':'💅','crown':'👑','round pushpin':'📍', \
+		'whale':'🐋','humster face':'🐹','sparkles':'✨','peach':'🍑','snowflake':'❄', \
+		'waving hand':'👋','raised hand':'✋','fire':'🔥','arrow down right':'⤵','chart upward trend':'📈', \
+		'chart downward trend':'📉','large blue circle':'🔵','princess':'👸','flower fleur-de-lis':'⚜','':'', \
+		'🚭':'no smoking','bomb':'💣','white up pointing':'☝','airplane':'✈','green apple':'🍏', \
+		'party popper':'🎉','vulcane salute':'🖖🏻','wrapped present':'🎁','dolphine':'🐬','fox face':'🦊', \
+		'oncoming car':'🚘','woman health worker':'👩🏻‍⚕','four leaf clower':'🍀','footprints':'👣','glowing star':'🌟', \
+		'bike':'🚲','bicyclist':'🚴','smiling face with sunglasses':'😎','person swimming':'🏊','sunglasses':'🕶', \
+		'surfing surf':'🏄','sunflower':'🌻','palm tree':'🌴','tropical drink':'🍹','desert island':'🏝', \
+		'sun':'☀','sun with face':'🌞','t-shirt':'👕','spiral shell':'🐚','sunflower':'🌻','cloud with rain':'🌧️', \
+		 'sunrise':'🌅','sunrise over mountains':'🌄','sunset':'🌇','bridge night':'🌉','train':'🚆', \
+		 'metro':'🚇','umbrella ground':'⛱'}
+
+emoji_food = {'Grapes':'🍇','Melon':'🍈','Watermelon':'🍉', 'Tangerine':'🍊', \
+ 'Lemon':'🍋', 'Banana':'🍌', 'Pineapple':'🍍', 'Red Apple':'🍎', 'Green Apple':'🍏', \
+ 'Pear':'🍐', 'Peach':'🍑', 'Cherries':'🍒', 'Strawberry':'🍓', 'Kiwi Fruit':'🥝', \
+ 'Tomato':'🍅', 'Coconut':'🥥', 'Avocado':'🥑', 'Eggplant':'🍆', 'Potato':'🥔', \
+ 'Carrot':'🥕', 'Ear of Corn':'🌽', 'Hot Pepper':'🌶', 'Cucumber':'🥒', 'Broccoli':'🥦', \
+ 'Mushroom':'🍄', 'Peanuts':'🥜', 'Chestnut':'🌰', 'Bread':'🍞', 'Croissant':'🥐', \
+ 'Baguette Bread':'🥖', 'Pretzel':'🥨', 'Pancakes':'🥞', 'Cheese Wedge':'🧀', 'Meat on Bone':'🍖', \
+ 'Poultry Leg':'🍗', 'Cut of Meat':'🥩', 'Bacon':'🥓', 'Hamburger':'🍔', 'French Fries':'🍟', \
+ 'Pizza':'🍕', 'Hot Dog':'🌭', 'Sandwich':'🥪', 'Taco':'🌮', 'Burrito':'🌯', 'Stuffed Flatbread':'🥙', \
+ 'Cooking':'🍳', 'Shallow Pan of Food':'🥘', 'Pot of Food':'🍲', 'Bowl With Spoon':'🥣', 'Green Salad':'🥗', \
+ 'Popcorn':'🍿', 'Canned Food':'🥫', 'Bento Box':'🍱', 'Rice Cracker':'🍘', 'Rice Ball':'🍙', \
+ 'Cooked Rice':'🍚', 'Curry Rice':'🍛', 'Steaming Bowl':'🍜', 'Spaghetti':'🍝', 'Roasted Sweet Potato':'🍠', \
+ 'Oden':'🍢', 'Sushi':'🍣', 'Fried Shrimp':'🍤', 'Fish Cake With Swirl':'🍥', 'Dango':'🍡', 'Dumpling':'🥟', \
+ 'Fortune Cookie':'🥠', 'Takeout Box':'🥡', 'Soft Ice Cream':'🍦', 'Shaved Ice':'🍧', 'Ice Cream':'🍨', \
+ 'Doughnut':'🍩', 'Cookie':'🍪', 'Birthday Cake':'🎂', 'Shortcake':'🍰', 'Pie':'🥧', 'Chocolate Bar':'🍫', \
+ 'Candy':'🍬', 'Lollipop':'🍭', 'Custard':'🍮', 'Honey Pot':'🍯', 'Baby Bottle':'🍼', 'Glass of Milk':'🥛', \
+ 'Hot Beverage':'☕', 'Teacup Without Handle':'🍵', 'Sake':'🍶', 'Bottle With Popping Cork':'🍾', 'Wine Glass':'🍷', \
+ 'Cocktail Glass':'🍸', 'Tropical Drink':'🍹', 'Beer Mug':'🍺', 'Clinking Beer Mugs':'🍻', 'Clinking Glasses':'🥂', \
+ 'Tumbler Glass':'🥃', 'Cup With Straw':'🥤', 'Chopsticks':'🥢', 'Fork and Knife With Plate':'🍽', \
+ 'Fork and Knife':'🍴','Spoon':'🥄'}
+
+emoji_summer = {'smiling face with sunglasses':'😎','surfing':'🏄','person swimming':'🏊','sunglasses':'🕶', \
+				'sunflower':'🌻','sun':'☀','sun with face':'🌞'}
+
+emoji_manicure = {'manicure':'💅','nail polish light skin tone':'💅🏻'}
+
+emoji_beauty_girl = {'lipstick':'💄','high-heeled shoe':'👠'}
+
+emoji_beach = {'umbrella ground':'⛱','beach with umbrella':'🏖','bikini':'👙','water wave':'🌊'}
+
+
+emoji_tbl = {'food':'emoji_food','foodporn':'emoji_food','еда':'emoji_food','summer':'emoji_summer', \
+			 'лето':'emoji_summer','солнце':'emoji_summer','nails':'emoji_manicure','manicure':'emoji_manicure', \
+			'лак':'emoji_manicure','гельлак':'emoji_manicure','маникюр':'emoji_manicure', \
+			'маник':'emoji_manicure','sexy':'emoji_beauty_girl','сексуально':'emoji_beauty_girl', \
+			'beach':'emoji_beach','пляж':'emoji_beach','море':'emoji_beach','отдых':'emoji_beach',}
